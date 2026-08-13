00a568569c92dc18615200768e7b765c52e4fd92
j-camposar <31163773+j-camposar@users.noreply.github.com>
2026-08-13 13:02:47 -0400

    V1.0.0: creacion de microservicio

---
+++ src/main/java/com/example/holaMundo/Controller/HolaMundoV1Controller.java
@@ -0,0 +1,19 @@
+package com.example.holaMundo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class HolaMundoV1Controller {
+    @GetMapping
+    public String holaMundo() {
+        return "hola mundo v1.0.0";
+    }
+}
