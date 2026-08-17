a61f65e68a6d461ce2202d3f5df52bd8af9df644
j-camposar <31163773+j-camposar@users.noreply.github.com>
2026-08-17 13:11:28 -0400

    V2.0.0 : Seguridad con auth2

--- src/main/java/com/example/holaMundo/Controller/HolaMundoV1Controller.java
+++ src/main/java/com/example/holaMundo/Controller/HolaMundoV1Controller.java
@@ -2,23 +2,27 @@ package com.example.holaMundo.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
 
 @RestController
 @RequestMapping("/api/v1")
 public class HolaMundoV1Controller {
     @GetMapping
-    public String holaMundo() {
-        return "hola mundo v1.0.0- bug corregido v1.1.1";
+    public String holaMundo(@AuthenticationPrincipal Jwt jwt) {
+        String usuario = jwt.getSubject();
+        return "hola mundo v1.0.0- bug corregido v1.1.1 "+usuario;
     }
     @PostMapping
-    public String despedida() {
-        return "despedida V1.1.0";
+    public String despedida(@AuthenticationPrincipal Jwt jwt) {
+        String usuario = jwt.getSubject();
+        return "despedida V1.1.0 "+usuario;
     }
-    
+
+    @GetMapping("/public")
+    public String endpointLibre() {
+        return "enspoint sin validacion";
+    }
 }
